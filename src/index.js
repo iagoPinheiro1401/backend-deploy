ef2a2d3bf58f18e24af6bdcb0c8852d42065c84d
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-02-07 16:31:10 -0300

    adicionado PORT

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors'
 import notesController from "./controllers/notes";
 
 const app = express();
-const port = 3333;
-
-const corsOptions = {
-  origin: 'http://localhost:3000',  // ou a origem do seu frontend
-};
-
-app.use(cors(corsOptions));
+const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/notes', notesController);
 
 app.listen(port, () => {
   console.log(`App rodando em http://localhost:${port}`);
 });
+
   
